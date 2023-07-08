1b301c5ed5f95fb0cdda4c5fab051fdc47aa9e5f
zwithe <zwithe133@gmail.com>
2023-07-08 11:17:53 -0400

    created server and get routes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+require('dotenv').config()
+const breadController = require('./controllers/bread')
+
+const app = express()
+
+app.use('/breads', breadController)
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
