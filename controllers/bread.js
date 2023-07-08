80f72c34d1402825ed5c341073ac3d55682c91eb
zwithe <zwithe133@gmail.com>
2023-07-08 13:15:01 -0400

    added react elements

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 router.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index', { breads: Bread })
 })
 router.get('/:index', (req, res) => {
     const { index } = req.params
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const breadController = require('./controllers/bread')
 
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+//routes
 app.use('/breads', breadController)
 
 const PORT = process.env.PORT
